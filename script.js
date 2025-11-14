6ef308a127c2df7ba949337bc6e12e4a7acce1ce
Zhanna <kezhanna.03@mail.ru>
2025-11-15 00:59:50 +0500

    tasks 5-6

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const checkButton = document.getElementById("check-btn");
-const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById('check-btn');
+const textInput = document.getElementById('text-input');
+const result = document.getElementById('result');
 
 function alertButton() {
-  if (textInput.value === "") {
+  if (textInput.value === '') {
     alert("Please input a value");
+  }
+  if(textInput.value==='A'){
+    result.innerText = "A is a palindrome";
+  }else if(textInput.value==='eye'){
+    result.innerText = "eye is a palindrome";
   }
 }
